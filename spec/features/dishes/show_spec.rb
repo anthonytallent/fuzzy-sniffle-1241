d0ca2add6c19c1058199a419f8525c5099fd65d6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-08 17:28:59 -0800

    Add test and route

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "dish show page" do
+  before :each do
+    @chef = Chef.create!(name: "Cookie Monster")
+    @dish = Dish.create!(name: "Mom's Spaghetti", description: "Made with love", calorie_count: 800, chef_id: @chef.id)
+
+    @ingredient_1 = Ingredient.create!(name: "Spaghetti Noodles",)
+    @ingredient_2 = Ingredient.create!(name: "Marinara Sauce")
+
+    DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient_1.id)
+    DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient_2.id)
+  end
   
+  it 'displays a dishes name and description' do
+    visit dish_path(@dish)
+
+    expect(page).to have_content(@dish.name)
+    expect(page).to have_content(@dish.description)
+  end
+
+  it 'displays the list of ingredients for that dish' do
+    visit dish_path(@dish)
+    within("#ingredients") do
+      expect(page).to have_content(@dish.ingredients.first.name)
+      expect(page).to have_content(@dish.ingredients.second.name)
+    end
+  end
+
+  it 'will display the chefs name' do
+    visit dish_path(@dish)
+save_and_open_page
+    expect(page).to have_content(@dish.chef.name)
+  end
 end
