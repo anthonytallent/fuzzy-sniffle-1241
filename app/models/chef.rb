8384780a207d6349b3d4dc863870fd51fb011b56
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-08 19:10:45 -0800

    completes us3

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,11 @@
 class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :dish_ingredients, through: :dishes
+  has_many :ingredients, through: :dish_ingredients
+
+  def chefs_unique_ingredients
+    ingredients.all.distinct
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dishes, only: [:show]
 
-  resources :chefs, only: [:show, :edit, :update]
+  resources :chefs, only: [:show, :edit, :update] do
+    resources :ingredients, only: [:index]
+  end
+
+  # resources :chef_ingredients, only: [:index]
 
   patch "/chefs/:id", to: "chefs#update"
 end
